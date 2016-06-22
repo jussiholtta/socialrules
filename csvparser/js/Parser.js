15d14ce2c491625d8f5f5466dbbe138dc93effd1
jussiholtta <jussi.holtta@gmail.com>
2016-06-22 20:55:05 +0300

    added parsing of a whole file to Card objects

--- csvparser/js/Parser.js
+++ csvparser/js/Parser.js
@@ -7,20 +7,34 @@ csvp.Parser.prototype.readTextArea = function(s) {
   return s.split('\n');
 }
 
-csvp.Parser.prototype.parseLine = function(arr) {  
+csvp.Parser.prototype.parseLine = function(arr) { 
   var l = arr.pop();
+  if(l[0] === "#") {
+    return "";
+  }
   return l.split('|');
 }
 
-csvp.Parser.prototype.createCard = function(arr) {
-  var card = {};
-  card.text = arr[0];
-  card.level = Number(arr[1]);
-  card.gm1 = Number(arr[2]) > 0;
-  card.gm2 = Number(arr[3]) > 0;
-  card.gm3 = Number(arr[4]) > 0;
-  card.amount = Number(arr[5]);
-  
-  return card;
+csvp.Parser.prototype.createCards = function(arr) {
+  var i;
+  var cards = [];
+  var length = arr.length;
+  for(i=0;i<length;i++) {
+    var l = this.parseLine(arr);
+    if(l != "") {
+      var c = new csvp.Card(l);
+      cards.push(c);
+    }
+  }
+  return cards;
+}
+
+csvp.Card = function(arr) {
+  this.text = arr[0];
+  this.level = Number(arr[1]);
+  this.amount = Number(arr[2]);
+  this.gm1 = Number(arr[3]) > 0;
+  this.gm2 = Number(arr[4]) > 0;
+  this.gm3 = Number(arr[5]) > 0;
 }
 
