b95ba06bd21933251f6ac997c8bcd896cb253ca2
jussiholtta <jussi.holtta@gmail.com>
2016-06-22 15:22:39 +0300

    started implementing google docs csvparser

---
+++ csvparser/js/Parser.js
@@ -0,0 +1,12 @@
+var csvp = csvp || {}
+
+csvp.Parser = function() {
+}
+
+csvp.Parser.prototype.readTextArea = function(s) {
+  //return null;
+}
+
+csvp.Parser.prototype.readLine = function(s) {  
+  return s;
+}
