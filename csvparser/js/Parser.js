41d58c8e652bcb7f9a4987c65ed3a3691459a55e
jussiholtta <jussi.holtta@gmail.com>
2016-06-22 19:04:14 +0300

    parsing skeleton done, output missing

--- csvparser/js/Parser.js
+++ csvparser/js/Parser.js
@@ -4,9 +4,23 @@ csvp.Parser = function() {
 }
 
 csvp.Parser.prototype.readTextArea = function(s) {
-  //return null;
+  return s.split('\n');
 }
 
-csvp.Parser.prototype.readLine = function(s) {  
-  return s;
+csvp.Parser.prototype.parseLine = function(arr) {  
+  var l = arr.pop();
+  return l.split('|');
 }
+
+csvp.Parser.prototype.createCard = function(arr) {
+  var card = {};
+  card.text = arr[0];
+  card.level = Number(arr[1]);
+  card.gm1 = Number(arr[2]) > 0;
+  card.gm2 = Number(arr[3]) > 0;
+  card.gm3 = Number(arr[4]) > 0;
+  card.amount = Number(arr[5]);
+  
+  return card;
+}
+
--- csvparser/test/csvParser-test.js
+++ csvparser/test/csvParser-test.js
@@ -2,7 +2,7 @@ var csvp = csvp || {}
 
 describe("csvp.Parser", function() {
     var parser = new csvp.Parser();
-    var myFile = "Modifier|Level|Pair #|New Team #|Carnival # |# in deck|Tested IRL\n\Blank|0|0|0|0|7|amount needs to scale with players to delay chaos, probably also need blanks on lower levels? current beta one subtle, 6 extreme\n\Ask the most quiet person to comment on the matter occasionally.|1||1|1|1|x\n\Stretch before speaking|1||1|1|1|x\n\Take a deep breath before answering|1|1|1|1|1|x\n\Take a deep breath after talking|1|1|1|1|1|x\n\Stare at someone else when replying to people|1|||1|1|x\n\Pick a person and mirror their posture and movements|1|1|1|1|1|\n\Keep your hands and legs crossed|1|1|1|1|1|\n\Keep your hands crossed|1|1|1|1|1|\n\Hold one hand in front of your mouth while others are speaking|1|||1|1|\n\Let someone else to start the discussion (WAS: Don't talk first)|1||1|1|1|(x)\n\Non-verbally agree with everything being said|1||1|1|1|\n\Start doing something else, but stay with the group.|1|1||1|1|x\n\Talk reaaaally sloowwwly|1|1||1|1|x\n\Wholeheartedly verbally agree with every 3rd comment|1||1|1|1|x\n\Stand up to talk|1|1|1|5|5|x (all as a 1, a bit extreme)\n\Keep your eyes closed while someone is talking|2|1||1|1|x\n\Start your responses with \"Yes, but...\"|2|1|||1|\n\Start your responses with \"Yes, and...\"|2|1|1|1|1|x\n\Always ask the people right next to you repeat what they say|2|||1|1|x (as 2 and 3, better as 2)\n\Apologize for not listening if someone tries to talk to you|2||1|1|1|x\n\Start a side discussion with someone else|2||1|2|2|x\n\Speak really fast|2|1|1|1|1|x\n\Thank each person who talks to you|2||1|1|1|x\n\Clap your hands together once before talking|2|1|1|1|1|\n\Stand up and sit down to talk|2|||1|1|x\n\Circle the group, but continue to participate|2|||1|1|x\n\Start a discussion in a different language|2||1|1|1|x\n\You can only talk if you interrupt someone|2|1|1|2|2|x\n\Leave the discussion, for example go get something to drink. Come back after 1 min and ask people to repeat what you missed.|3|1|1|1|1|x\n\Turn away from the group when you speak|3|1||1|1|x\n\Change your seat after talking|3||1|1|1|exists, but has never been played\n\Chaos Monkey - do whatever you can without harming people to disrupt the system|3|||1|1|x\n\Try to do the exact opposite of all the rules|3|1|1|1|1|x\n"
+    var myFile = "Modifier|Level|Pair #|New Team #|Carnival # |# in deck|Tested IRL\n\Blank|0|0|0|0|7|amount needs to scale with players to delay chaos, probably also need blanks on lower levels? current beta one subtle, 6 extreme\n\Ask the most quiet person to comment on the matter occasionally.|1||1|1|1|x\n\Stretch before speaking|1||1|1|1|x\n\Take a deep breath before answering|1|1|1|1|1|x\n\Take a deep breath after talking|1|1|1|1|1|x\n\Stare at someone else when replying to people|1|||1|1|x\n\Pick a person and mirror their posture and movements|1|1|1|1|1|\n\Keep your hands and legs crossed|1|1|1|1|1|\n\Keep your hands crossed|1|1|1|1|1|\n\Hold one hand in front of your mouth while others are speaking|1|||1|1|\n\Let someone else to start the discussion (WAS: Don't talk first)|1||1|1|1|(x)\n\Non-verbally agree with everything being said|1||1|1|1|\n\Start doing something else, but stay with the group.|1|1||1|1|x\n\Talk reaaaally sloowwwly|1|1||1|1|x\n\Wholeheartedly verbally agree with every 3rd comment|1||1|1|1|x\n\Stand up to talk|1|1|1|5|5|x (all as a 1, a bit extreme)\n\Keep your eyes closed while someone is talking|2|1||1|1|x\n\Start your responses with \"Yes, but...\"|2|1|||1|\n\Start your responses with \"Yes, and...\"|2|1|1|1|1|x\n\Always ask the people right next to you repeat what they say|2|||1|1|x (as 2 and 3, better as 2)\n\Apologize for not listening if someone tries to talk to you|2||1|1|1|x\n\Start a side discussion with someone else|2||1|2|2|x\n\Speak really fast|2|1|1|1|1|x\n\Thank each person who talks to you|2||1|1|1|x\n\Clap your hands together once before talking|2|1|1|1|1|\n\Stand up and sit down to talk|2|||1|1|x\n\Circle the group, but continue to participate|2|||1|1|x\n\Start a discussion in a different language|2||1|1|1|x\n\You can only talk if you interrupt someone|2|1|1|2|2|x\n\Leave the discussion, for example go get something to drink. Come back after 1 min and ask people to repeat what you missed.|3|1|1|1|1|x\n\Turn away from the group when you speak|3|1||1|1|x\n\Change your seat after talking|3||1|1|1|exists, but has never been played\n\Chaos Monkey - do whatever you can without harming people to disrupt the system|3|||1|1|x\n\Try to do the exact opposite of all the rules|3|1|1|1|1|x"
     beforeEach(function(){
       });
 
@@ -15,11 +15,28 @@ describe("csvp.Parser", function() {
 
     it("is able to read a textArea to an array of lines", function() {
       var arr = parser.readTextArea(myFile);
-      expect(arr).not.toBeNull(); 
+      expect(arr[1]).not.toBeNull();
+      expect(arr.length).toEqual(35);
+      expect(arr[1][0]).toEqual('B');
       }); 
 
     it("is able to read a line", function() {
-      expect(function(){parser.readLine("abc")}).not.toThrow();
+      var arr = parser.readTextArea(myFile);
+      var s = parser.parseLine(arr);
+      expect(s[0]).toEqual("Try to do the exact opposite of all the rules");
+      expect(s[5]).toEqual("1");
+    });
+
+    it("is able to create a card out of a line", function() {
+        var arr = parser.readTextArea(myFile);
+        var s = parser.parseLine(arr);
+        var card = parser.createCard(s);
+        expect(card.text).toEqual("Try to do the exact opposite of all the rules");
+        expect(card.level).toEqual(3);
+        expect(card.gm1).toBe(true);
+        expect(card.gm2).toBe(true);
+        expect(card.gm3).toBe(true);
+        expect(card.amount).toEqual(1);
     });
 
 });
