5414938db721eb6d2dadd626d15809d78b2abd5e
jussiholtta <jussi.holtta@gmail.com>
2016-07-02 15:54:12 +0300

    added new icons, adjusted colors

--- cardcreator/update_icons.sh
+++ cardcreator/update_icons.sh
@@ -2,7 +2,7 @@
 FILES=images/iconsvg/*
 for f in $FILES
 do
-  /Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "$(pwd)/${f%.svg}.png" --export-text-to-path -d300 "$(pwd)/$f"
+  /Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "$(pwd)/${f%.svg}.png" -C --export-text-to-path -d1200 "$(pwd)/$f"
 done
 
 mv images/iconsvg/*png images/
