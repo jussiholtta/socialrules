421dece2d28d24a334f86525751bd9676c2c63df
jussiholtta <jussi.holtta@gmail.com>
2016-07-22 16:02:53 +0300

    removed unneccesary step of moving files in create cards script, switched to png for convenience while constantly updating cards

--- cardcreator/create_cards_svg.sh
+++ cardcreator/create_cards_svg.sh
@@ -3,7 +3,8 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-mkdir -p svgtmp
+mkdir -p svgtmp/
+mkdir -p final
 
 #just copy files to avoid mess with static paths in svg
 cp images/* svgtmp/
@@ -23,14 +24,17 @@ while read modifier level gamemode1 gamemode2 gamemode3; do
 done <cards.csv
 IFS=$OLDIFS
 
-FILES=svgtmp/*svg
+#
+cd svgtmp
+FILES=*svg
 for f in $FILES
 do
-  #/Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-eps "$(pwd)/output.eps" -d300 "$(pwd)/front_template.svg"
-  /Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-eps "$(pwd)/$f.eps" --export-text-to-path "$(pwd)/$f"
+#  /Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-eps "$(pwd)/../final/$f.eps" --export-text-to-path "$(pwd)/$f"
+  /Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-png "$(pwd)/../final/$f.png" --export-text-to-path -d300 "$(pwd)/$f"
 done
-mkdir -p final
-mv svgtmp/*.eps final/
-#rsync -av final/ ~/Google\ Drive/Work/_Rules/cards
+cd ..
+#
+
 rm svgtmp/*
+rsync -av final/ ~/Google\ Drive/Work/_Rules/cards
 
